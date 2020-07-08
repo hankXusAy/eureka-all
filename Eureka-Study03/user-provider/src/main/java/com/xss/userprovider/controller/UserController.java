fe67c60a67220d881d4ef4e3cece7af01a888658
xushaoshuai <xu.shaoshuai@cesgroup.com.cn>
2020-07-08 14:57:32 +0800

    服务的超时和重试机制

--- Eureka-Study03/user-provider/src/main/java/com/xss/userprovider/controller/UserController.java
+++ Eureka-Study03/user-provider/src/main/java/com/xss/userprovider/controller/UserController.java
@@ -28,18 +28,22 @@ public class UserController implements UserAPI {
 
     @Override
     public String alive() {
+        /*所有的超时时间设置和重试次数和负载设置,都是在客户端设置,服务提供方只提供服务,
+        调用某个节点失败,6s之内不会再调取这个节点(有可能是因为网络的原因导致的,不会下线服务,6s后还是会访问这个节点),
+        但是如果服务已经死了,6s后还是会调用这个节点,因此不够灵活,也不是最好的方案*/
 
         try {
             System.out.println("准备睡");
-
-            Thread.sleep(500);
+            Thread.sleep(4000);
+//            Thread.sleep(400);
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
         int i = count.getAndIncrement();
-        System.out.println("====好的第：" + i + "次调用");
+        System.out.println(port + "====坏的第：" + i + "次调用");
+//        System.out.println(port + "====好的第：" + i + "次调用");
         return "port:" + port;
     }
 
