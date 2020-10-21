646705c051162f13bef241c56b36e6bcdd5b81c0
xushaoshuai <xu.shaoshuai@cesgroup.com.cn>
2020-10-21 16:24:34 +0800

    ....

--- Eureka-Study04/zuul-test/src/main/java/com/xss/zuultest/ZuulTestApplication.java
+++ Eureka-Study04/zuul-test/src/main/java/com/xss/zuultest/ZuulTestApplication.java
@@ -8,7 +8,8 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class ZuulTestApplication {
 
-    /*通过主机名+服务名称(spring.application.name)+方法名,zuul会将该请求转发到后台的consumer上,帮我们完成一次服务的中转*/
+    /*通过主机名+服务名称(spring.application.name)+方法名,
+    zuul会将该请求转发到后台的consumer上,帮我们完成一次服务的中转*/
     public static void main(String[] args) {
         SpringApplication.run(ZuulTestApplication.class, args);
     }
